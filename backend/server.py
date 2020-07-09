f63625e907a4c8eaff974f96b2af7d255ed47264
Jake Carr <jakecarr@Jakes-Mac-mini.local>
2020-07-09 14:04:08 -0500

    skeleton code for adding players to games

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, redirect, request, session, url_for
+import os
 
 class Application:
     def __init__(self, static_folder, webport):
@@ -9,13 +10,36 @@ class Application:
 
     def create_flask_app(self, static_folder):
         app = Flask(__name__, static_folder = static_folder)
+        app.secret_key = os.urandom(16)
+
+        @app.route('/login', methods = ['GET', 'POST'])
+        def login():
+            if request.method == 'POST':
+                session['username'] = request.form['username']
+                return redirect(url_for('join'))
+            return app.send_static_file('login.html')
+        
+        @app.route('/join', methods = ['GET', 'POST'])
+        def join():
+            if request.method == 'POST':
+                if len(request.form) != 0:
+                    gameId = request.form.get('gameId')
+                else:
+                    gameId = self._gameCount
+                print('adding player %s to game %s' % (session['username'], gameId))
+                return redirect(url_for('play'))
+            return app.send_static_file('join.html')
+        
+        @app.route('/play', methods = ['GET'])
+        def play():
+            return app.send_static_file('play.html')
 
         @app.route('/api/create_game', methods = ['GET'])
         def create_game():
-            msg = 'creating game # %s' % self._gameCount
+            msg = 'creating game #%s for user %s' % (self._gameCount, session['username'])
             self._gameCount += 1
             print(msg)
-            return jsonify(msg)
+            return redirect(url_for('play'))
 
         @app.route('/')
         @app.route('/<path:path>')
@@ -23,7 +47,7 @@ class Application:
             if path:
                 return app.send_static_file(path)
             else:
-                return app.send_static_file('index.html')
+                return redirect(url_for('login'))
         
         return app
     
