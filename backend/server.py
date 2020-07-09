0a88ef8a6e5fc217dfb53515d78aaf9bb11bb722
Jake Carr <jakecarr@Jakes-Mac-mini.local>
2020-07-09 15:01:43 -0500

    improve robustness of addPlayer logic and error handling

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,34 @@
 from flask import Flask, jsonify, redirect, request, session, url_for
 import os
 
+class Game:
+    def __init__(self, gameId):
+        self._players = set()
+        self._gameId = gameId
+
+    def addPlayer(self, username):
+        if username in self._players:
+            raise ValueError( "Username %s already playing this game." % username )
+        if len(self._players) >= 4:
+            raise ValueError( "Game #%s is full." % self._gameId )
+        self._players.add(username)
+
 class Application:
     def __init__(self, static_folder, webport):
         self._app = self.create_flask_app(static_folder)
         self._webport = webport
         self._games = []
         self._gameCount = 0
+    
+    def joinGame(self, gameId, username):
+        if gameId >= len(self._games):
+            return 'Cannot join game #%s. Game does not exist.' % gameId
+        self._games[gameId].addPlayer(username)
+
+    def createGame(self, gameId, username):
+        self._games.append(Game(gameId))
+        self._games[gameId].addPlayer(username)
+        self._gameCount += 1
 
     def create_flask_app(self, static_folder):
         app = Flask(__name__, static_folder = static_folder)
@@ -22,10 +44,22 @@ class Application:
         @app.route('/join', methods = ['GET', 'POST'])
         def join():
             if request.method == 'POST':
-                if len(request.form) != 0:
-                    gameId = request.form.get('gameId')
-                else:
-                    gameId = self._gameCount
+                username = session['username']
+                try:
+                    if len(request.form) != 0:
+                        # join game
+                        gameId = int(request.form.get('gameId'))
+                        retMsg = self.joinGame(gameId, username)
+                    else:
+                        # create game
+                        gameId = self._gameCount
+                        retMsg = self.createGame(gameId, username)
+                    
+                    if retMsg:
+                        return "Error: %s" % retMsg
+                except Exception as e:
+                    return "Error: %s" % str( e )
+
                 print('adding player %s to game %s' % (session['username'], gameId))
                 return redirect(url_for('play'))
             return app.send_static_file('join.html')
@@ -33,13 +67,17 @@ class Application:
         @app.route('/play', methods = ['GET'])
         def play():
             return app.send_static_file('play.html')
-
-        @app.route('/api/create_game', methods = ['GET'])
-        def create_game():
-            msg = 'creating game #%s for user %s' % (self._gameCount, session['username'])
-            self._gameCount += 1
-            print(msg)
-            return redirect(url_for('play'))
+        
+        @app.route('/api/update_game_state', methods = ['POST'])
+        def update_game_state():
+            return jsonify('updating game state')
+        
+        @app.route('/api/check_game_state', methods = ['GET'])
+        def check_game_state():
+            msg = "There are %s games. " % len(self._games)
+            for gameId in range(len(self._games)):
+                msg += "Game %s has players %s. " % (gameId, str(self._games[gameId].players))
+            return jsonify(msg)
 
         @app.route('/')
         @app.route('/<path:path>')
